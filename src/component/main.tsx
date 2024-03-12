58e8d4b0ab915dd3ef753a2d48ca4406e04a4e7a
Kim-Yun-Seo <rlagrace410@gmail.com>
2024-03-12 12:07:31 +0900

    [feat] 랜덤 json 추가

--- src/component/main.tsx
+++ src/component/main.tsx
@@ -1,6 +1,5 @@
 import {
-    Button,
-    SimpleGrid, Stack, Tab, TabList, TabPanel, TabPanels, Tabs
+    Button, Stack
 } from '@chakra-ui/react'
 
 import {OneImage} from "./props/oneImage.tsx";
@@ -10,41 +9,54 @@ import {useState} from "react";
 
 export const Main = () => {
     const imageList = []
-    for (let i = 0; i < 20; i++) {
-        imageList.push(
-            {
-                "id": 1234,
-                "name": "loppy",
-                "desc": "loopy pic",
-                "category": "character",
-                "tags": ["loopy","japan"],
-                "madeBy": "kim",
-                "createdAt": "2024-12-12",
-                "imgUrl": `https://source.unsplash.com/random/?${i}`,
-                "prompt": "loopy, red hat, arm",
-                "negativePrompt": "bad quality",
-                "inputImage": "",
-                "controlNetModel": "1.5controlnet",
-                "controlNetType": "lineart",
-                "LoRA": "loopy LoRA",
-                "checkpointModel": "sdxl"
-            }
-        )
-    }
     const categoryL = []
-
     images.forEach((image: any, idx: number) => {
         if (!categoryL.includes(image.category)) {
             categoryL.push(image.category)
         }
     })
+    const makeImageList = () => {
+        for (let i = 0; i < 20; i++) {
+            imageList.push(
+                {
+                    "id": 1234,
+                    "name": "loppy",
+                    "desc": "loopy pic",
+                    "category": categoryL[getRandomIntInclusive(0, categoryL.length-1)],
+                    "tags": ["loopy","japan"],
+                    "madeBy": "kim",
+                    "createdAt": "2024-12-12",
+                    "imgUrl": `https://source.unsplash.com/random/?${i}`,
+                    "prompt": "loopy, red hat, arm",
+                    "negativePrompt": "bad quality",
+                    "inputImage": "",
+                    "controlNetModel": "1.5controlnet",
+                    "controlNetType": "lineart",
+                    "LoRA": "loopy LoRA",
+                    "checkpointModel": "sdxl"
+                }
+            )
+        }
+    }
+    makeImageList()
+
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
+    }
+
+    let [tempImgList, setTempImgList] = useState(imageList) // = images
+
+    //tempImgList를 필터를 씌워서 만들어주고
+    //그걸 가지고 보여주기만 한다.
 
     const categoryBtn = categoryL.map((category) => <Button colorScheme='teal' onClick={() => {
-        return setImageCard(images.filter((image: any) => image.category === category).map((image) => (<OneImage name={image.name} url={image.imgUrl} id={image.id} desc={image.desc} date={image.createdAt} tags={image.tags}/>)))
+        tempImgList = imageList.filter((image: any) => image.category === category)
     }}>{category}</Button>)
 
     let [imageCard, setImageCard] = useState(
-        imageList.map((image: any) => (<div><OneImage name={image.name} url={image.imgUrl} id={image.id} desc={image.desc} date={image.createdAt} tags={image.tags}/></div>))
+        tempImgList.map((image: any) => (<div><OneImage name={image.name} url={image.imgUrl} id={image.id} desc={image.desc} date={image.createdAt} tags={image.tags}/></div>))
     )
 
     return (
